a52e6204fa3d377319823eb70357e9aa4e6d1bf1
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-09-16 21:10:25 +0430

    fix: change main arch for clean arch

--- src/main.go
+++ src/main.go
@@ -1,23 +1,12 @@
 package main
 
 import (
-	"os"
-
-	"github.com/gin-gonic/gin"
 	"github.com/mojtabaRKS/link_shortener/internal/webserver"
-	"github.com/mojtabaRKS/link_shortener/pkg/db"
-	"github.com/mojtabaRKS/link_shortener/pkg/env"
 )
 
-func init() {
-	env.Load()
-	db.Load()
-}
-
 func main() {
-	routesHandler := func(r *gin.Engine) *gin.Engine {
-		return webserver.Routes(r)
-	}
+	app := webserver.App{}
 
-	webserver.RunServer(routesHandler, os.Getenv("PORT"))
+	app.Initilize()
+	app.RunServer()
 }
