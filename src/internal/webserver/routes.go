afc11bf6448f8e6c88291272cb7cf8522b7217ed
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-09-16 21:11:10 +0430

    fix: refactor routes and use gorilla router

--- src/internal/webserver/routes.go
+++ src/internal/webserver/routes.go
@@ -1,12 +1,16 @@
 package webserver
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func Routes(r *gin.Engine) *gin.Engine {
-	rg := r.RouterGroup.Group("/api/v1")
+	auth_v1 "github.com/mojtabaRKS/link_shortener/internal/webserver/controllers/v1/auth"
+)
+
+func (app *App) InitilizeRoutes() {
+	r := app.Router.PathPrefix("/api").Subrouter() 
+	
+	r.PathPrefix("/v1").Subrouter()
 	{
-		rg.POST("register", auth_v1.Register)
+		r.HandleFunc("register", auth_v1.Register).Methods(http.MethodPost)
 	}
-
-	return r
 }
