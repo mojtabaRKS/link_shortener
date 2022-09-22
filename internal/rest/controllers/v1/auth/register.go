7a89db529205558e68723ec50815facdae3d272e
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-09-22 21:59:21 +0330

    feat: complete rest register controller

--- internal/rest/controllers/v1/auth/register.go
+++ internal/rest/controllers/v1/auth/register.go
@@ -1,30 +1,64 @@
 package auth
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"github.com/mojtabaRKS/link_shortener/internal/rest/util"
+	AuthService "github.com/mojtabaRKS/link_shortener/internal/services/auth"
 	"github.com/mojtabaRKS/link_shortener/pkg/validation"
-	// "github.com/mojtabaRKS/link_shortener/pkg/util"
 )
 
 type RegisterRequest struct {
-	Email    string `json:"email" validate:"required,email,unique"`
+	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,min=8"`
 	Name     string `json:"name" validate:"required"`
-	UserName string `json:"username" validate:"required,unique"`
+	UserName string `json:"username" validate:"required"`
 }
 
+type TokenStruct struct{
+	Token string `json:"token"`
+}
 
 func Register(w http.ResponseWriter, r *http.Request) {
-	
-	// errs, request := validation.Validate(r, &RegisterRequest{})
-	validation.Validate(r, &RegisterRequest{})
+	req := RegisterRequest{}
+	errs, _ := validation.Validate(r, &req)
 
-	// if len(errs) != 0 {
-		// util.RespondValidationError(c, errs)
-		// return
-	// }
+	if len(errs) != 0 {
+		util.RespondValidationError(w, errs)
+		return
+	}
 
-	// x := r.Body
-	// w.Write(x)
-	
+	token, err := AuthService.Register(
+		req.Name,
+		req.UserName,
+		req.Email,
+		req.Password,
+	)
+
+	if err != nil {
+		util.RespondError(w, err)
+		return
+	}
+
+	res := util.MainStructure{
+		Meta: util.Meta{
+			Success: true,
+			Code: http.StatusCreated,
+			Message: "user registered successfully !",
+		},
+		Data: TokenStruct{Token: token},
+	}
+
+	marshalled, err := json.Marshal(res)
+
+	if err != nil {
+		util.RespondError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintln(w, string(marshalled))
+	return
 }
