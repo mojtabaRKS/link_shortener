7f9bb7405f223314c32b58cc336203d1909d19de
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-09-22 21:58:03 +0330

    fix : create server and use in main app

--- cmd/link_shortener/main.go
+++ cmd/link_shortener/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"github.com/mojtabaRKS/link_shortener/internal/rest"
+	"github.com/mojtabaRKS/link_shortener/internal/bootstrap"
 )
 
 func main() {
-	app := rest.App{}
+	server := &bootstrap.Server{}
 
-	app.Initilize()
-	app.RunServer()
+	// load REST server
+	rest_app := &rest.App{Server: server}
+	rest_app.Initilize()
+	rest_app.RunServer()
+
+	// load GRPC server
 }
